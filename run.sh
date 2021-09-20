9d8c9e561c07abc7b407b4f8955afa3fbf3056b1
Watchanon Numnam <jiramot@gmail.com>
2021-09-20 16:02:02 +0700

    Add cors plugin

--- run.sh
+++ run.sh
@@ -5,11 +5,13 @@ docker build -t kong-demo --no-cache .
 docker run -ti --rm --name kong-go-plugins \
 -e "KONG_DATABASE=off" \
 -e "KONG_DECLARATIVE_CONFIG=/tmp/config.yml" \
--e "KONG_PLUGINS=bundled,open-api" \
+-e "KONG_PLUGINS=bundled,cors,open-api" \
 -e "KONG_PLUGINSERVER_NAMES=open-api" \
 -e "KONG_PLUGINSERVER_OPEN_API_START_CMD=/usr/local/bin/open-api" \
 -e "KONG_PLUGINSERVER_OPEN_API_QUERY_CMD=/usr/local/bin/open-api -dump" \
 -e "KONG_PROXY_LISTEN=0.0.0.0:8000" \
+-e "KONG_ADMIN_LISTEN=0.0.0.0:8001" \
 -e "KONG_LOG_LEVEL=debug" \
 -p 8000:8000 \
+-p 8001:8001 \
  kong-demo
